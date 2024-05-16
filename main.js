967ed4c291f17a932f79efaa10ba3823401fd60e
David Shaw <dshaw11@utk.edu>
2024-05-16 15:27:36 -0400

    added if statements to ensure a url is entered

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-
+import { argv } from'node:process'
+import { normalizeURL, getURLsFromHTML } from "./crawl.js"
 
 function main() {
-
+    if (argv.length >= 4) {
+        console.error('Too Many Arugments');
+        return;
+    }
+    if (argv.length < 3) {
+        console.error('Please enter a URL');
+        return;
+    }
+    let array = getURLsFromHTML(argv[2])
+    console.log(array)
 }
 
 main()
